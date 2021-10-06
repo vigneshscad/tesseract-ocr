c2e63cb3edc96e976103a9d773919a8add796716
vigneshscad <91971019+vigneshscad@users.noreply.github.com>
2021-10-06 19:55:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract' #for heroku setup
 st.set_option('deprecation.showfileUploaderEncoding',False)           #avoid warnings
 st.title("OCR  - Optical Character recognition")
-st.text("Upload the image")
+st.text("Upload the image") 
 
 uploaded_file = st.sidebar.file_uploader("Choose an image",type = {"jpg","jpeg","png"})
 if(uploaded_file is not None):
